6f89d3931453fd5e1355bcb21a40437cb62765ed
dlyons5 <dominic.lyons@patelhs.org>
2020-10-13 18:08:18 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class Main {
+  
+  public static void main(String[] args) {
+
+    Scanner scan = new Scanner(System.in); 
+    
+    System.out.println("choose a number between 1 and 5");
+    int number = scan.nextInt();
+  
+    System.out.println("choose a number that adds to equal 12 (so like what + the number you choose = 12");
+    int number2 = scan.nextInt();
+  }
+}
